87b6d086bf4e3a22d7a23430ae376cc88c490f7f
Toby Ho & Wendy Yang <atlwendy@gmail.com>
2014-04-18 15:13:15 -0400

    test models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :meeting_memberships
   has_many :meetings, through: :meeting_memberships
 end
--- test/models/meeting_test.rb
+++ test/models/meeting_test.rb
@@ -4,4 +4,15 @@ class MeetingTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @meeting = meetings(:one)
+    @bob = users(:one)
+  end
+
+  test "can associate to user" do
+    assert_difference("MeetingMembership.count", 1) do
+      @meeting.users = [@bob]
+    end
+  end
 end
