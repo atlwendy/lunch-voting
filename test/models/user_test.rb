1b7a2e60658a203df06b8d60d3be1ee097d41007
Future Ho <atlwendy@gmail.com>
2014-05-15 15:34:23 -0400

    add user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,16 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "create a new user" do
+  	newuser = User.new
+  	newuser.email = 'x@abc'
+  	newuser.password = 'thisisatest'
+  	newuser.save
+  	assert_equal newuser.errors.messages[:email][0], "is invalid"
+  	newuser.email = 'x@abc.com'
+  	newuser.password = 'hello'
+  	newuser.save
+  	assert_equal newuser.errors.messages[:password][0], "is too short (minimum is 6 characters)" 
+  end
 end
