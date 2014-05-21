dda722c4748c32c8269dfb8058599289e0777a9e
Future Ho <atlwendy@gmail.com>
2014-05-21 11:32:17 -0400

    update integration basic flow test for remembering session cookies.

--- test/integration/basic_flows_test.rb
+++ test/integration/basic_flows_test.rb
@@ -16,6 +16,12 @@ class BasicFlowsTest < ActionDispatch::IntegrationTest
   test "edit meeting members" do
     Capybara.current_driver = Capybara.javascript_driver
     visit('/meetings')
+
+    assert page.has_content?("Sign in")
+    fill_in 'session_email', with: 'bob@bob.com'
+    fill_in 'session_password', with: 'testbob'
+    click_button('Sign in')
+
     click_link('Show')
     assert_equal current_path, meeting_path(@meeting)
     assert page.has_content?('First Lunch')
@@ -38,6 +44,12 @@ class BasicFlowsTest < ActionDispatch::IntegrationTest
   test "edit restaurants" do
     Capybara.current_driver = Capybara.javascript_driver
     visit('/meetings')
+
+    assert page.has_content?("Sign in")
+    fill_in 'session_email', with: 'bob@bob.com'
+    fill_in 'session_password', with: 'testbob'
+    click_button('Sign in')
+
     click_link('Show')
     click_link('edit restaurant')
     assert_equal current_path, "#{meeting_path(@meeting)}/select_restaurants"
@@ -54,14 +66,23 @@ class BasicFlowsTest < ActionDispatch::IntegrationTest
   end
 
   test "edit user" do
+    Capybara.current_driver = Capybara.javascript_driver
     visit('/users')
+
+    assert page.has_content?("Sign in")
+    fill_in 'session_email', with: 'bob@bob.com'
+    fill_in 'session_password', with: 'testbob'
+    click_button('Sign in')
+
     click_link('Show')
     assert_equal current_path, user_path(@user)
     assert page.has_content?('Annie')
     click_link('Edit')
     assert_equal current_path, edit_user_path(@user)
-    fill_in('Username', :with => 'Marty')
+    fill_in 'user_username', with: 'Marty'
+    sleep 3
     click_button('Update User')
+    sleep 2
     assert_equal current_path, user_path(@user)
     assert page.has_content?('Marty')
   end
@@ -69,6 +90,12 @@ class BasicFlowsTest < ActionDispatch::IntegrationTest
   test "link to meeting from user" do
     @user.meetings = [@meeting]
     visit("/users/#{@user.id}")
+
+    assert page.has_content?("Sign in")
+    fill_in 'session_email', with: 'bob@bob.com'
+    fill_in 'session_password', with: 'testbob'
+    click_button('Sign in')
+
     click_link('First Lunch')
     assert_equal current_path, meeting_path(@meeting)
   end
@@ -76,6 +103,11 @@ class BasicFlowsTest < ActionDispatch::IntegrationTest
   test "edit meeting" do
     Capybara.current_driver = Capybara.javascript_driver
     visit('/meetings')
+    assert page.has_content?("Sign in")
+    fill_in 'session_email', with: 'bob@bob.com'
+    fill_in 'session_password', with: 'testbob'
+    click_button('Sign in')
+
     click_link('Edit')
 
     assert_equal current_path, edit_meeting_path(@meeting)
