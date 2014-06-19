9fb35b084eace7fcaae3f479910292f913510f56
Toby Ho & Wendy Yang <atlwendy@gmail.com>
2014-06-19 10:15:12 -0700

    Fixed bootstrap issue by adding bootstrap.js manually.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,8 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+  $("a[rel~=popover], .has-popover").popover()
+  $("a[rel~=tooltip], .has-tooltip").tooltip()
+})
