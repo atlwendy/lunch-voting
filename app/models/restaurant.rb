1e07cb137d8c958a2018e159d4fdecde26eb5a28
Future Ho <atlwendy@gmail.com>
2014-06-21 21:53:16 -0400

    enforce http added to the url

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,9 +2,16 @@ class Restaurant < ActiveRecord::Base
   has_many :meeting_restaurant_selections
   has_many :meetings, :through => :meeting_restaurant_selections
 
-  before_save :capitalize_name
+  before_save :capitalize_name, :url_add_http
 
   def capitalize_name
   	self.name.capitalize!
   end
+
+  def url_add_http
+  	if not self.url.include?("http")
+  		self.url = "http://" + self.url
+  	end
+  end
+
 end
