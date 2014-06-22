5199647f8ec8b8ce6224db74a2b58468b7a71df8
Future Ho <atlwendy@gmail.com>
2014-06-21 21:43:42 -0400

    Enforce restaurant name to be capitalized, and put restaurants on the top menu

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,7 +5,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants
   # GET /restaurants.json
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.all.order('lower(name)')
   end
 
   # GET /restaurants/1
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,10 @@
 class Restaurant < ActiveRecord::Base
   has_many :meeting_restaurant_selections
   has_many :meetings, :through => :meeting_restaurant_selections
+
+  before_save :capitalize_name
+
+  def capitalize_name
+  	self.name.capitalize!
+  end
 end
