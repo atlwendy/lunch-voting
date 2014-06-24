f82d0003be726acfde202abb7caccea8fc406918
Future Ho <atlwendy@gmail.com>
2014-06-23 22:15:00 -0400

    more styling for meetings show page glyphicons first try

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Lunchvoting
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
+    config.assets.compile = true
+    
   end
 end
