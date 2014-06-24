bace55ac4c36aae698f22b13c99067b9ea3de51d
Future Ho <atlwendy@gmail.com>
2014-06-24 17:02:18 -0400

    user index user show styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,5 +15,9 @@ module ApplicationHelper
   	  gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
   	  image_tag(gravatar_url, alt: user.username, class: "gravatar")
   	end
-
+  	
+  	def gravatar_url(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  	end
 end
