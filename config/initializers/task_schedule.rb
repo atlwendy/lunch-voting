2d2339f7d458ac5a8087fc12bb4c60fa0109ffe6
Wendy Ho <atlwendy@gmail.com>
2014-09-17 23:23:57 -0400

    version fix

--- config/initializers/task_schedule.rb
+++ config/initializers/task_schedule.rb
@@ -1,7 +1,7 @@
 require 'rubygems'  
 require 'rufus/scheduler'
 
-scheduler = Rufus::Scheduler.start_new
+scheduler = Rufus::Scheduler.new
 
 scheduler.cron("* 0,8,16 * * *") do  
     `rake "send_reminder_email --trace 2>&1"`
