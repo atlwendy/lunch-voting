e9337d68fb11eb806cf8cae13e3dbdf80e5e99de
Toby Ho <airportyh@gmail.com>
2014-11-10 15:17:25 -0500

    Redesigned the frontpage.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140911200915) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "invitations", force: true do |t|
     t.string   "recipient_email"
     t.string   "string"
